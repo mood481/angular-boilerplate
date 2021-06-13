31eb3dcd7ea18efca6260e1ce953be37ebf02500
JD Cáceres Tapia <jdct@macto.es>
2021-06-13 19:03:00 +0200

    feat: added i18n support using transloco lib

---
+++ src/app/app.constants.ts
@@ -0,0 +1,16 @@
+
+
+export class AppConstants
+{
+    public static readonly LANGS = [
+        {
+            id: "es",
+            label: "Español"
+        },
+        {
+            id: "em",
+            label: "English"
+        }
+    ];
+    public static DEFAULT_LANG = "es";
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,15 @@ import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 import localeEs from "@angular/common/locales/es";
 import {SimpleLoggerModule, SimpleLogLevel} from "@macto/ngx-simple-logger";
+import { TRANSLOCO_CONFIG, TRANSLOCO_LOADER, TranslocoConfig,
+    TranslocoModule, TranslocoService } from "@ngneat/transloco";
 
 registerLocaleData(localeEs);
 
 import { environment } from "@abo/environments/environment";
 import { LoggerService } from "./services/logger.service";
+import { TranslateHttpLoader } from "@abo/core/services/translate-http.loader";
+import { AppConstants } from "@abo/app.constants";
 
 
 @NgModule({
@@ -19,20 +23,35 @@ import { LoggerService } from "./services/logger.service";
         HttpClientModule,
         SimpleLoggerModule.forRoot({
             level: !environment.production ? SimpleLogLevel.DEBUG : SimpleLogLevel.ERROR
-        })
+        }),
+        TranslocoModule
     ],
     exports: [
         BrowserModule,
         CommonModule,
         HttpClientModule,
+        TranslocoModule
     ],
     providers: [
+        {
+            provide: TRANSLOCO_CONFIG,
+            useValue: {
+                availableLangs: AppConstants.LANGS,
+                defaultLang: environment.defaultLang || AppConstants.DEFAULT_LANG,
+                fallbackLang: environment.defaultLang || AppConstants.DEFAULT_LANG,
+                prodMode: environment.production,
+                flatten: {
+                    aot: environment.production
+                }
+            } as TranslocoConfig
+        },
+        {provide: TRANSLOCO_LOADER, useClass: TranslateHttpLoader},
         LoggerService
     ]
 })
 export class CoreModule
 {
-    public constructor() {
-
+    public constructor(protected translocoService: TranslocoService) {
+        this.translocoService.setActiveLang(environment.defaultLang || AppConstants.DEFAULT_LANG);
     }
 }
